ebf564319879d279c7ffa3b7f55edca2e32a36cb
Jacob <jianxunji@gmail.com>
2021-06-17 01:53:13 +0800

    Change public to external for gas saving

--- src/TokenAmountFromDex.sol
+++ src/TokenAmountFromDex.sol
@@ -217,7 +217,7 @@ contract Ownable is Context {
     /**
      * @dev Returns the address of the current owner.
      */
-    function owner() public view returns (address) {
+    function owner() external view returns (address) {
         return _owner;
     }
 
@@ -236,7 +236,7 @@ contract Ownable is Context {
      * NOTE: Renouncing ownership will leave the contract without an owner,
      * thereby removing any functionality that is only available to the owner.
      */
-    function renounceOwnership() public virtual onlyOwner {
+    function renounceOwnership() external virtual onlyOwner {
         emit OwnershipTransferred(_owner, address(0));
         _owner = address(0);
     }
@@ -245,7 +245,7 @@ contract Ownable is Context {
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
      * Can only be called by the current owner.
      */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
+    function transferOwnership(address newOwner) external virtual onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
@@ -271,7 +271,7 @@ contract TokenAmountFromDex is Ownable {
     }
     
     // Update the harvest fee ratio
-    function setPairContract(DexPairLike _pairContract) public onlyOwner {
+    function setPairContract(DexPairLike _pairContract) external onlyOwner {
         require(_pairContract != DexPairLike(0), "pairContract can not be zero!");
         pairContract = _pairContract;
         emit SetPairContract(_pairContract);
